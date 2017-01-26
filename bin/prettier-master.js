d4462bf96b2f952317b5693ccf44af9b72dc20dd
Gordon Dent <gmd@fb.com>
2017-01-26 10:35:59 -0800

    Prettifying of JS for f8ab682783157e2efc5a9381b7f482b8f85c6a69

--- bin/prettier-master.js
+++ bin/prettier-master.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
+var fs = require("fs");
+var path = require("path");
+var execFileSync = require("child_process").execFileSync;
 
-var fs = require('fs');
-var path = require('path');
-var execFileSync = require('child_process').execFileSync;
-
-var prompt = 'prettier-master';
-var masterBranch = process.env.MASTER_BRANCH || 'master';
-var prettierCommand = process.env.PRETTIER_CMD || 'prettier';
+var prompt = "prettier-master";
+var masterBranch = process.env.MASTER_BRANCH || "master";
+var prettierCommand = process.env.PRETTIER_CMD || "prettier";
 
 var isCI = !!process.env.CI;
 var isTravis = !!process.env.TRAVIS;
@@ -16,7 +15,7 @@ var isCircle = !!process.env.CIRCLE;
 
 var cwd = null;
 function exec(command, args) {
-  console.error('>', [command].concat(args).join(' '));
+  console.error(">", [ command ].concat(args).join(" "));
   var options = {};
   if (cwd) {
     options.cwd = cwd;
@@ -25,54 +24,54 @@ function exec(command, args) {
 }
 
 function ensureGitUserExists(repoSlug) {
-  repoSlug = repoSlug || '[repoSlug]';
+  repoSlug = repoSlug || "[repoSlug]";
   if (!process.env.GITHUB_USER) {
     console.error(
-      'In order to use ' + prompt + ', you need to configure a ' +
-      'few environment variables to be able to commit to the ' +
-      'repository. Follow those steps to get you setup:\n' +
-      '\n' +
-      'Go to https://github.com/settings/tokens/new\n' +
-      ' - Fill "Token description" with "' + prompt + ' for ' +
-        repoSlug + '"\n' +
-      ' - Check "public_repo"\n' +
-      ' - Press "Generate Token"\n' +
-      '\n' +
-      'In a different tab, go to https://travis-ci.org/' +
-        repoSlug + '/settings\n' +
-      ' - Make sure "Build only if .travis.yml is present" is ON\n' +
-      ' - Fill "Name" with "GITHUB_USER" and "Value" with the name of the ' +
+      "In order to use " +
+        prompt +
+        ", you need to configure a " +
+        "few environment variables to be able to commit to the " +
+        "repository. Follow those steps to get you setup:\n" +
+        "\n" +
+        "Go to https://github.com/settings/tokens/new\n" +
+        ' - Fill "Token description" with "' +
+        prompt +
+        " for " +
+        repoSlug +
+        '"\n' +
+        ' - Check "public_repo"\n' +
+        ' - Press "Generate Token"\n' +
+        "\n" +
+        "In a different tab, go to https://travis-ci.org/" +
+        repoSlug +
+        "/settings\n" +
+        ' - Make sure "Build only if .travis.yml is present" is ON\n' +
+        ' - Fill "Name" with "GITHUB_USER" and "Value" with the name of the ' +
         'account you generated the token with. Press "Add"\n' +
-      '\n' +
-      'Once this is done, commit anything to the repository to restart ' +
-        'Travis and it should work :)'
+        "\n" +
+        "Once this is done, commit anything to the repository to restart " +
+        "Travis and it should work :)"
     );
     process.exit(1);
   }
 
-  exec(
-    'git',
-    [
-      'config',
-      '--global',
-      'user.name',
-      process.env.GITHUB_USER_NAME || 'facts-tracker',
-    ]
-  );
-  exec(
-    'git',
-    [
-      'config',
-      '--global',
-      'user.email',
-      process.env.GITHUB_USER_EMAIL || 'facts-tracker@no-reply.github.com',
-    ]
-  );
+  exec("git", [
+    "config",
+    "--global",
+    "user.name",
+    process.env.GITHUB_USER_NAME || "facts-tracker"
+  ]);
+  exec("git", [
+    "config",
+    "--global",
+    "user.email",
+    process.env.GITHUB_USER_EMAIL || "facts-tracker@no-reply.github.com"
+  ]);
 }
 
 function ensureGitIsClean() {
-  if (exec('git', ['status', '--porcelain'])) {
-    console.error(prompt + ': `git status` is not clean, aborting.');
+  if (exec("git", [ "status", "--porcelain" ])) {
+    console.error(prompt + ": `git status` is not clean, aborting.");
     process.exit(1);
   }
 }
@@ -82,7 +81,7 @@ function getRepoSlug() {
     return process.env.TRAVIS_REPO_SLUG;
   }
 
-  var remotes = exec('git', ['remote', '-v']).split('\n');
+  var remotes = exec("git", [ "remote", "-v" ]).split("\n");
   for (var i = 0; i < remotes.length; ++i) {
     var match = remotes[i].match(/^origin\t[^:]+:([^\.]+).+\(fetch\)/);
     if (match) {
@@ -90,7 +89,7 @@ function getRepoSlug() {
     }
   }
 
-  console.error('Cannot find repository slug, sorry.');
+  console.error("Cannot find repository slug, sorry.");
   process.exit(1);
 }
 
@@ -100,13 +99,13 @@ function getBranch() {
   } else if (isCircle) {
     return process.env.CIRCLE_BRANCH;
   } else {
-    return exec('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
+    return exec("git", [ "rev-parse", "--abbrev-ref", "HEAD" ]);
   }
 }
 
 function ensureBranchIsMaster(branch) {
   if (branch !== masterBranch) {
-    console.log(prompt + ': Branch is not master, exiting...');
+    console.log(prompt + ": Branch is not master, exiting...");
     process.exit(0);
   }
 }
@@ -114,44 +113,46 @@ function ensureBranchIsMaster(branch) {
 function ensureNotPullRequest() {
   if (
     !!process.env.TRAVIS_PULL_REQUEST ||
-    !!process.env.CI_PULL_REQUEST ||
-    !!process.env.CI_PULL_REQUESTS
+      !!process.env.CI_PULL_REQUEST ||
+      !!process.env.CI_PULL_REQUESTS
   ) {
-    console.error(prompt + ': This is a PR, exiting...');
+    console.error(prompt + ": This is a PR, exiting...");
     process.exit(0);
   }
 }
 
 function getCommitHash() {
-  return exec('git', ['rev-parse', 'HEAD']).trim();
+  return exec("git", [ "rev-parse", "HEAD" ]).trim();
 }
 
 function getJSFilesChanged(commitHash) {
-  var diff = exec(
-    'git',
-    ['diff-tree', '--no-commit-id', '--name-only', '-r', commitHash]
-  );
-  return diff.trim()
-    .split(/\s+/g)
-    .filter(function(file) {
-      return file.substring(file.length - 3) === '.js'
-    });
+  var diff = exec("git", [
+    "diff-tree",
+    "--no-commit-id",
+    "--name-only",
+    "-r",
+    commitHash
+  ]);
+  return diff.trim().split(/\s+/g).filter(function(file) {
+    return file.substring(file.length - 3) === ".js";
+  });
 }
 
 function runPrettier(jsFiles) {
   try {
-    exec(prettierCommand, ['--write'].concat(jsFiles));
-  } catch(e) {
-    if (prettierCommand === 'prettier') {
+    exec(prettierCommand, [ "--write" ].concat(jsFiles));
+  } catch (e) {
+    if (prettierCommand === "prettier") {
       console.error(
-        'It looks like Prettier is not installed globally. You\'ll need to ' +
-        'run `npm install -g prettier` and make sure it installs successfully'
+        "It looks like Prettier is not installed globally. You'll need to " +
+          "run `npm install -g prettier` and make sure it installs successfully"
       );
     } else {
       console.error(
-        'It looks like Prettier is not installed at the path you specified: ' +
-        prettierCommand + '. Please double check this or alternatively ' +
-        'install globally with `npm install -g prettier`.'
+        "It looks like Prettier is not installed at the path you specified: " +
+          prettierCommand +
+          ". Please double check this or alternatively " +
+          "install globally with `npm install -g prettier`."
       );
     }
     process.exit(1);
@@ -159,30 +160,28 @@ function runPrettier(jsFiles) {
 }
 
 function getLastCommitAuthor() {
-  return exec('git', ['log', '-1', '--format="%an <%ae>"']).trim();
+  return exec("git", [ "log", "-1", '--format="%an <%ae>"' ]).trim();
 }
 
 function updateGitIfChanged(commitHash) {
-  var noFilesChanged = exec('git', ['status', '--porcelain'])
+  var noFilesChanged = exec("git", [ "status", "--porcelain" ])
     .trim()
-    .split('\n')
-    .length;
+    .split("\n").length;
   if (noFilesChanged > 0) {
-    exec('git', ['add', '--all']);
-    exec(
-      'git', [
-        'commit',
-        '-m',
-        'Prettifying of JS for ' + commitHash,
-        '--author="' + getLastCommitAuthor() + '"',
-      ]);
-    exec('git', ['push', 'origin', masterBranch]);
+    exec("git", [ "add", "--all" ]);
+    exec("git", [
+      "commit",
+      "-m",
+      "Prettifying of JS for " + commitHash,
+      '--author="' + getLastCommitAuthor() + '"'
+    ]);
+    exec("git", [ "push", "origin", masterBranch ]);
     var outcome = noFilesChanged === 1
-      ? '1 file prettified!'
-      : noFilesChanged + 'files prettified!';
-    console.error(prompt + ': ' + outcome);
+      ? "1 file prettified!"
+      : noFilesChanged + "files prettified!";
+    console.error(prompt + ": " + outcome);
   } else {
-    console.error(prompt + ': nothing to update');
+    console.error(prompt + ": nothing to update");
   }
 }
 
@@ -198,7 +197,7 @@ var commitHash = getCommitHash();
 var jsFilesChanged = getJSFilesChanged(commitHash);
 
 if (jsFilesChanged.length === 0) {
-  console.error(prompt + ': nothing to update');
+  console.error(prompt + ": nothing to update");
   process.exit(0);
 }
 
